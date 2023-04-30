a150a1f73c372f04ed690bbb15dddc72cb2dede3
User <nazarovtolya2@gmail.com>
2023-04-30 10:39:46 +0500

    НАчало дз

---
+++ src/test/java/com/example/homework2_4/Homework24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework2_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
